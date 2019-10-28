c8b19ab5b7e8e2cf259d62a78cd48bdb94d7a5af
Leocario <nicolas.constantin@students.hevs.ch>
2019-10-28 11:36:00 +0100

    add orders on DTO

---
+++ DTO/orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class orders
+    {
+        public int id { get; set; }
+        public String status { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime delivery_time { get; set; }
+        public int customers_id { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id}|{status}|{created_at}|{delivery_time}|{customers_id}";
+        }
+    }
+}
