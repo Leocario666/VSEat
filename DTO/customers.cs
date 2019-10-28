ed83f4bcb8b770ddf9dc3307a2e32c51a6d8f8fd
yogan14 <dalmeidiog@hotmail.com>
2019-10-28 10:56:02 +0100

    add customers to DTO

---
+++ DTO/customers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class customers
+    {
+
+        public int id { get; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public DateTime created_at { get; }
+        public int city_code { get; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public override string ToString()
+        {
+            return $"{id}|{first_name}";
+        }
+    }
+   
+}
