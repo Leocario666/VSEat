bc47b885556888989b6933cf0f7dab22f10b006f
yogan14 <dalmeidiog@hotmail.com>
2019-10-28 11:35:40 +0100

    Add restaurants to DTO

---
+++ DTO/restaurants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class restaurants
+    {
+        public int id { get; }
+        public string name { get; }
+        public DateTime created_at { get; }
+        public int city_code { get; }
+
+        public override string ToString()
+        {
+            return $"{id}|{name}|{created_at}|{city_code}";
+        }
+    }
+}
