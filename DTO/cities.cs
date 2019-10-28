6bbfceb721538c182e7bbc690dbd64fd624107fd
Leocario <nicolas.constantin@students.hevs.ch>
2019-10-28 10:50:22 +0100

    add delivery_staff to DTO

---
+++ DTO/cities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class cities
+    {
+        public int code { get;  }
+        public String name { get; }
+
+        public override string ToString()
+        {
+            return $"{code}|{name}";
+        }
+
+    }
+}
---
+++ DTO/delivery_staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class delivery_staff
+    {
+        public int id { get; }
+        public String firstname { get; }
+        public String lastname { get; }
+        public int city_code { get; }
+        public String login { get; }
+        public String password { get; }
+
+        public override string ToString()
+        {
+            return $"{id}|{firstname}|{lastname}|{city_code}|{login}|{password}";
+        }
+    }
+}
