1500b66c3fbb2723b83d74920688e46e1114efa0
Leocario <nicolas.constantin@students.hevs.ch>
2019-12-02 11:45:26 +0100

    restaurant

--- DTO/restaurants.cs
+++ DTO/restaurants.cs
@@ -8,7 +8,6 @@ namespace DTO
     {
         public int restaurant_Id { get; set; }
         public string name { get; set; }
-        public DateTime created_at { get; set; }
         public int cityCode { get; set; }
     }
 }
