22a924235ef0cd9cea93cc9abe5e9437688f6754
yogan14 <dalmeidiog@hotmail.com>
2019-12-07 16:01:53 +0100

    Essai du RestaurantController

--- A_WebAppEat/Controllers/RestaurantController.cs
+++ A_WebAppEat/Controllers/RestaurantController.cs
@@ -2,32 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BLL;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace A_WebAppEat.Controllers
 {
-    public class RestaurantsController : Controller
+    public class RestaurantController : Controller
     {
-        // GET: Restaurants
+        public IRestaurantManager RestaurantManager { get; }
+        public RestaurantController(IRestaurantManager restaurantManager)
+        {
+            RestaurantManager = restaurantManager;
+        }
+        // GET: Restaurant
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Restaurants/Details/5
+        // GET: Restaurant/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var restaurant = RestaurantManager.GetRestaurant(id);
+            return View(restaurant);
         }
 
-        // GET: Restaurants/Create
+        // GET: Restaurant/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Restaurants/Create
+        // POST: Restaurant/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -44,13 +51,13 @@ namespace A_WebAppEat.Controllers
             }
         }
 
-        // GET: Restaurants/Edit/5
+        // GET: Restaurant/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Restaurants/Edit/5
+        // POST: Restaurant/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -67,13 +74,13 @@ namespace A_WebAppEat.Controllers
             }
         }
 
-        // GET: Restaurants/Delete/5
+        // GET: Restaurant/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Restaurants/Delete/5
+        // POST: Restaurant/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
