640313cec5ecc0fcd1a83ca13791a354ef2f7c3c
Leocario <nicolas.constantin@students.hevs.ch>
2019-12-14 17:18:41 +0100

    attribution des delivery staff success

--- A_WebAppEat/Controllers/RestaurantController.cs
+++ A_WebAppEat/Controllers/RestaurantController.cs
@@ -74,10 +74,12 @@ namespace A_WebAppEat.Controllers
             IOrderDB orderdb = new OrderDB(Configuration);
             IOrder_dishesDB orderddb = new Order_dishesDB(Configuration);
             IDelivery_staffDB dldb = new Delivery_staffDB(Configuration);
+            IRestaurantDB restodb = new RestaurantDB(Configuration);
             IDishManager dishManager = new DishManager(dish);
             IOrderManager orderManager = new OrderManager(orderdb);
             IOrder_dishesManager order_DishesManager = new Order_dishesManager(orderddb);
             IDelivery_staffManager dlmanager = new Delivery_staffManager(dldb);
+            IRestaurantManager restoManager = new RestaurantManager(restodb);
 
 
             List<Dish_Order> dishlist = new List<Dish_Order>();
@@ -126,12 +128,98 @@ namespace A_WebAppEat.Controllers
 
                 var dslist = dlmanager.GetDelivery_staffs();
                 int idDs = -1;
+                var listeresto = restoManager.GetRestaurants();
+                int cityCodeTemp = 0;
+                
 
-                foreach(Delivery_staff ds in dslist)
+                foreach (Delivery_staff ds in dslist)
                 {
-                    if(ds.cityCode == 1974)
+
+                    foreach(Dish d in plat)
+                    { 
+                        if (d.dish_Id == dishlist[0].Dish_Id)
+                        {
+                            foreach (Restaurant r in listeresto)
+                            {
+                                if (d.restaurant_Id == r.restaurant_Id)
+                                {
+                                    cityCodeTemp = r.cityCode;
+                                }
+                            }
+                        }
+                    }
+
+                    if (ds.cityCode == cityCodeTemp)
                     {
                         var isbusy = false;
+                        var oListByDl = orderManager.GetOrders_ds(ds.delivery_staff_Id);
+                        int cptOrder = 0;
+                        foreach (Order o in oListByDl)
+                        {
+                             if(o.status == "non delivery")
+                            {
+                                string deliveryTimePourInt = o.delivery_time.Remove(1,1);
+                                string deliveryTimePartOne = deliveryTimePourInt.Substring(0,1);
+                                string deliveryTimePartTwo = deliveryTimePourInt.Substring(2);
+                                int test = Int32.Parse(deliveryTimePartTwo);
+                                switch (test)
+                                {
+                                    case 00:
+                                        break;
+
+                                    case 15:
+                                        test = 25;
+                                        break;
+                                    case 30:
+                                        test = 50;
+                                        break;
+                                    case 45:
+                                        test = 75;
+                                        break;
+                                }
+                                deliveryTimePartTwo = test.ToString();
+
+                                int deliveryTimeInt = Int32.Parse(deliveryTimePartOne + deliveryTimePartTwo);
+
+                                string deliveryTimeForCompare = deliveryTime.Remove(1, 1);
+
+                                string dtPartOne = deliveryTimeForCompare.Substring(0, 1);
+                                string dtPartTwo = deliveryTimeForCompare.Substring(2);
+                                int essai = Int32.Parse(deliveryTimePartTwo);
+                                switch (essai)
+                                {
+                                    case 00:
+                                        break;
+
+                                    case 15:
+                                        test = 25;
+                                        break;
+                                    case 30:
+                                        test = 50;
+                                        break;
+                                    case 45:
+                                        test = 75;
+                                        break;
+                                }
+                                dtPartTwo = test.ToString();
+                                int dTForCompare = Int32.Parse(dtPartOne + dtPartTwo);
+                                int compare = (deliveryTimeInt - dTForCompare);
+
+                                if(compare <= 50 || compare >= -50)
+                                {
+                                    cptOrder++;
+                                }
+
+
+                            }
+                        }
+
+                        if(cptOrder == 5)
+                        {
+                            isbusy = true;
+                        }
+
+
                         if (isbusy == false)
                         {
                             idDs = ds.delivery_staff_Id;
@@ -139,24 +227,35 @@ namespace A_WebAppEat.Controllers
                     }
                 }
 
-                ikse.delivery_staff_Id = idDs;
-
-                var orderaiedi = orderManager.AddOrder(ikse);
-
-                foreach (Dish_Order disho in dishlist)
+                if(idDs != -1)
                 {
-                    Order_dishes order_dishes_ = new Order_dishes();
-                    order_dishes_.order_Id = orderaiedi;
-                    order_dishes_.dish_Id = disho.Dish_Id;
-                    order_dishes_.quantity = disho.Quantity;
-                    order_dishes_.price = disho.totalPrice;
-                    if(order_dishes_.quantity != 0)
+                    ikse.delivery_staff_Id = idDs;
+
+                    var orderaiedi = orderManager.AddOrder(ikse);
+
+                    foreach (Dish_Order disho in dishlist)
                     {
-                        order_DishesManager.AddOrder_dishes(order_dishes_);
+                        Order_dishes order_dishes_ = new Order_dishes();
+                        order_dishes_.order_Id = orderaiedi;
+                        order_dishes_.dish_Id = disho.Dish_Id;
+                        order_dishes_.quantity = disho.Quantity;
+                        order_dishes_.price = disho.totalPrice;
+                        if (order_dishes_.quantity != 0)
+                        {
+                            order_DishesManager.AddOrder_dishes(order_dishes_);
+                        }
                     }
+
+                    return View();
                 }
+                
             }
 
+            return RedirectToAction("error", "Restaurant");
+        }
+
+        public ActionResult error()
+        {
             return View();
         }
 
